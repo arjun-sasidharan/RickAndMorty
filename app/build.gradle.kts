c4b750d0e166ff89305b3cfcd4833081065ca388
Arjun <71964107+arjun-sasidharan@users.noreply.github.com>
2023-08-09 13:48:37 +0530

    Setup Retrofit, Moshi Converter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,18 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
+    // Retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // Moshi
+    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation ("com.squareup.moshi:moshi-kotlin:1.12.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
