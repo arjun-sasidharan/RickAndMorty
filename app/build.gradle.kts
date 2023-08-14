5949cf0cdea504bdd8ab137fbc4e904682865bfc
Arjun <71964107+arjun-sasidharan@users.noreply.github.com>
2023-08-14 22:59:57 +0530

    Update chucker version to enable notification on Android 13

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,8 +54,8 @@ dependencies {
 
     // Networking
     implementation ("com.squareup.okhttp3:logging-interceptor:4.11.0")
-    debugImplementation ("com.github.chuckerteam.chucker:library:3.5.2")
-    releaseImplementation ("com.github.chuckerteam.chucker:library-no-op:3.5.2")
+    debugImplementation ("com.github.chuckerteam.chucker:library:4.0.0")
+    releaseImplementation ("com.github.chuckerteam.chucker:library-no-op:4.0.0")
 
     // Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
