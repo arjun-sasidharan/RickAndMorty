c78cb3e2e34df506fe5cda22f7cffd9f14705450
Arjun <71964107+arjun-sasidharan@users.noreply.github.com>
2023-08-16 10:19:41 +0530

    Add appbar support

--- app/src/main/java/com/example/rickandmorty/NavGraphActivity.kt
+++ app/src/main/java/com/example/rickandmorty/NavGraphActivity.kt
@@ -2,10 +2,18 @@ package com.example.rickandmorty
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 
 class NavGraphActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,6 +22,18 @@ class NavGraphActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+//      val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(
+            navController = navController,
+            configuration = appBarConfiguration
+        )
 
+
+//        findViewById<NavigationView>(R.id.navView).setupWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
