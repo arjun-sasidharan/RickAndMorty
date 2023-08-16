7167ccbfdc4ef7590cafeba60f1ac1706368766a
Arjun <71964107+arjun-sasidharan@users.noreply.github.com>
2023-08-16 11:03:15 +0530

    Add Drawer functionality - add new fragment for episodes list

--- app/src/main/java/com/example/rickandmorty/NavGraphActivity.kt
+++ app/src/main/java/com/example/rickandmorty/NavGraphActivity.kt
@@ -6,6 +6,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
@@ -22,18 +23,22 @@ class NavGraphActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-//      val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        appBarConfiguration = AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.characterListFragment, R.id.episodeListFragment),
+            drawerLayout = drawerLayout
+        )
         setupActionBarWithNavController(
             navController = navController,
             configuration = appBarConfiguration
         )
 
 
-//        findViewById<NavigationView>(R.id.navView).setupWithNavController(navController)
+        findViewById<NavigationView>(R.id.navView).setupWithNavController(navController)
     }
+
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
