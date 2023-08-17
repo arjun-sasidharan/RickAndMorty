e09261dd4c53d5e6edf8a26c3f0d34b19ee26ba9
Arjun <71964107+arjun-sasidharan@users.noreply.github.com>
2023-08-17 23:12:57 +0530

    Update character detail view model and repository to match mvvm pattern

--- app/src/main/java/com/example/rickandmorty/SharedRepository.kt
+++ app/src/main/java/com/example/rickandmorty/SharedRepository.kt
@@ -3,12 +3,20 @@ package com.example.rickandmorty
 import com.example.rickandmorty.domain.mappers.CharacterMapper
 import com.example.rickandmorty.domain.models.Character
 import com.example.rickandmorty.network.NetworkLayer
+import com.example.rickandmorty.network.RickAndMortyCache
 import com.example.rickandmorty.network.response.GetCharacterByIdResponse
 import com.example.rickandmorty.network.response.GetEpisodeByIdResponse
 
 class SharedRepository {
 
     suspend fun getCharacterById(characterId: Int): Character? {
+
+        // Check the cache for character
+        val cachedCharacter = RickAndMortyCache.characterMap[characterId]
+        if (cachedCharacter != null) {
+            return cachedCharacter
+        }
+
         val request = NetworkLayer.apiClient.getCharacterById(characterId)
 
         if (request.failed || !request.isSuccessful) {
@@ -17,11 +25,14 @@ class SharedRepository {
 
         // fetching episode
         val networkEpisodes = getEpisodesFromCharacterResponse(request.body)
-
-        return CharacterMapper.buildFrom(
+        val character = CharacterMapper.buildFrom(
             response = request.body,
             episodes = networkEpisodes
         )
+
+        // Update cache
+        RickAndMortyCache.characterMap[characterId] = character
+        return character
     }
 
     private suspend fun getEpisodesFromCharacterResponse(
--- app/src/main/java/com/example/rickandmorty/SharedViewModel.kt
+++ app/src/main/java/com/example/rickandmorty/SharedViewModel.kt
@@ -5,38 +5,23 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.rickandmorty.domain.models.Character
-import com.example.rickandmorty.network.RickAndMortyCache
 import kotlinx.coroutines.launch
 
 class SharedViewModel : ViewModel() {
 
     private val repository = SharedRepository()
 
+    // region comment: why two mutable live data
     // reason for having mutable live data and regular live data is bcs, we do not want whatever layer is listening
     // to this live data also have the ability to change it, we just want them to be able to consume the information.
     // so that only from the inside of the shared view model we should be able to modify the underlining data in the _characterByIdLiveData
+    // endregion
+
     private val _characterByIdLiveData = MutableLiveData<Character?>()
     val characterByIdLiveData: LiveData<Character?> = _characterByIdLiveData
 
-    fun fetchCharacter(characterId: Int) {
-
-        // Check the cache for character
-        val cachedCharacter = RickAndMortyCache.characterMap[characterId]
-        if (cachedCharacter != null) {
-            _characterByIdLiveData.postValue(cachedCharacter)
-            return
+    fun fetchCharacter(characterId: Int) = viewModelScope.launch {
+            val character = repository.getCharacterById(characterId)
+            _characterByIdLiveData.postValue(character)
         }
-
-        // Otherwise, fetch character from api
-        viewModelScope.launch {
-            val response = repository.getCharacterById(characterId)
-            _characterByIdLiveData.postValue(response)
-
-            // Update cache if non null character received
-            response?.let {
-                RickAndMortyCache.characterMap[characterId] = it
-            }
-        }
-    }
-
 }
